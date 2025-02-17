9c7bcaf16178f27138e0dd6b284d331a9c9c3a42
Alba Ma <alba.maydeu@gmail.com>
2025-02-17 13:12:16 +0100

    Update HolaMundo.java

--- c2-javabasics/src/HolaMundo.java
+++ c2-javabasics/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo 2025");
+		System.out.println("Hola Mundo va bien el commit?");
 	}
 
 }
