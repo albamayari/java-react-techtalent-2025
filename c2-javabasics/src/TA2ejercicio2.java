8adeee235cea7e0cb167df042a2ee82a69320c5b
Alba Ma <alba.maydeu@gmail.com>
2025-02-18 12:40:17 +0100

    Create TA2ejercicio2.java

---
+++ c2-javabasics/src/TA2ejercicio2.java
@@ -0,0 +1,10 @@
+
+public class TA2ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
