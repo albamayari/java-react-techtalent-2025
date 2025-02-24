f381d125561222deb1a9a963ef2a4947e31d4b0e
Alba Ma <alba.maydeu@gmail.com>
2025-02-24 12:26:52 +0100

    Create TA4ejercicio1.java

---
+++ c2-javabasics/src/TA4ejercicio1.java
@@ -0,0 +1,23 @@
+
+public class TA4ejercicio1 {
+
+	public static void main(String[] args) {
+	    // Declaración de 2 variables de tipo numérica
+	    int numero1 = 15;
+	    int numero2 = 7;
+
+	    // Operaciones aritméticas y mostrar resultados en pantalla
+	    System.out.println("El resultado de la suma de " + numero1 
+	            + " más " + numero2 + " es igual a " + (numero1 + numero2) 
+	            + "\nResta: " + (numero1 - numero2) 
+	            + "\nMultiplicación: " + (numero1 * numero2) 
+	            + "\nDivisión: " + (numero1 / numero2) 
+	            + "\nMódulo: " + (numero1 % numero2));
+
+	    // Las siguientes líneas están comentadas y no se ejecutan
+	    // System.out.println(numero1 + numero2);
+	    // System.out.println(numero1 - numero2);
+	    // System.out.println(numero1 * numero2);
+	    // System.out.println(numero1 / numero2);
+	    // System.out.println(numero1 % numero2);
+	}
