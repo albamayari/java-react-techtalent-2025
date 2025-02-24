6da86c5eec9c1d149a3134d11e150f0d44c60db8
Alba Ma <alba.maydeu@gmail.com>
2025-02-24 12:57:18 +0100

    Update TA4ejercicio2.java

--- c2-javabasics/src/TA4ejercicio2.java
+++ c2-javabasics/src/TA4ejercicio2.java
@@ -2,7 +2,28 @@
 public class TA4ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declaración e inicialización de variables
+				int N = 5;
+				double A = 4.56;
+				char C = 'a';
+				char D = '&';
+
+				// Muestra el valor de cada variable
+				System.out.println("Variable N = " + N);
+				System.out.println("Variable A = " + A);
+				System.out.println("Variable C = " + C);
+				
+//				double resultadoSuma = N + A;
+
+				// Muestra la suma de N + A y la diferencia de A - N
+				System.out.println(N + " + " + A + " = " + (N + A));
+				System.out.println(A + " - " + N + " = " + (A - N));
+
+				// Muestra el valor numérico correspondiente al carácter en la variable C
+				System.out.println("Valor numérico del carácter " + C + " = " + (int) C);
+//				https://elcodigoascii.com.ar/ Tabla ASCII
+				
+				System.out.println("Ejemplo de & en ASCII es " + (int) D);
 
 	}
 
