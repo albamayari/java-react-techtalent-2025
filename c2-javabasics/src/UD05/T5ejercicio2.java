4fe5221cb9b5658937b08d755d592213e514547f
Alba Ma <alba.maydeu@gmail.com>
2025-02-28 13:55:24 +0100

    Update T5ejercicio2.java

--- c2-javabasics/src/UD05/T5ejercicio2.java
+++ c2-javabasics/src/UD05/T5ejercicio2.java
@@ -4,6 +4,12 @@ package UD05;
 public class T5ejercicio2 {
 
     public static void main(String[] args) {
-        
+    	// Declara un String que contenga tu nombre, después muestra
+		// un mensaje de bienvenida por consola.
+		// Por ejemplo: si introduzco “Alba”,
+		// me aparezca “Bienvenida Alba”
+        String nombre = "Alba";
+        System.out.println("Bienvenida guapissima " + nombre);
+       
     }
 }
