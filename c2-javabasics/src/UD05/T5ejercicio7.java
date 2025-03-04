bd8e8b49eb0e5aea9c287952e5b0d044cfab4a6c
Alba Ma <alba.maydeu@gmail.com>
2025-03-04 12:22:22 +0100

    Create T5ejercicio7.java

---
+++ c2-javabasics/src/UD05/T5ejercicio7.java
@@ -0,0 +1,13 @@
+package UD05;
+
+public class T5ejercicio7 {
+	
+	public static void main(String[] args) {
+        
+        system.out.println(interador);
+        interador++;
+}
+	
+	int i=0;// iniciacion normal de ?intergres ' enteros
+	system.out.println(i);
+	while
