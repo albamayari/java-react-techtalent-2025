ca41124da28bb6eb6ccf77f3d7830ceb089cd31c
Alba Ma <alba.maydeu@gmail.com>
2025-03-18 12:50:12 +0100

    ejercio 8 unidad 5

--- c2-javabasics/src/UD05/T5ejercicio07.java
+++ c2-javabasics/src/UD05/T5ejercicio07.java
@@ -3,7 +3,9 @@ package UD05;
 public class T5ejercicio07 {
 	
 	public static void main(String[] args) {
-    //EL ejercicio me pide que Muestra los números del 1 al 100 (ambos incluidos). el ejercio me pide que use un bucle while    
+    //EL ejercicio me pide que Muestra los números del 1 al 100 (ambos incluidos). el ejercio me pide que use un bucle while   
+		//while es una estructura de control que se utiliza para repetir un bloque de código mientras se cumple una condición. La condición se evalúa antes de ejecutar el bloque de código. Si la condición es verdadera, el bloque de código se ejecuta. Si la condición es falsa, el bloque de código no se ejecuta y el programa continúa con la instrucción siguiente después del bucle while.
+		//Es decir, el bucle while se ejecutará mientras que la condición sea verdadera. Por lo tanto, es importante asegurarse de que la condición se vuelva falsa en algún momento para evitar un bucle infinito.Como ejemplo de bucle while, vamos a mostrar los números del 1 al 100 en la consola. Para ello, vamos a seguir los siguientes pasos:
 		
 	//Paso 1- Declarar una variable entera para el bucle while es decir un contador que empiece en 1 y se llame interador. Un interador es una variable que se utiliza para contar el número de veces que se ejecuta un bucle.
 		//La palabra  interador vine de la palabra inglesa "iterator" que significa "iterador" en español que signfica "recorrer" en español.
@@ -18,6 +20,8 @@ public class T5ejercicio07 {
 	//Paso 4- Incrementar el valor del interador en 1.
         interador++; // interador++ es lo mismo que interador = interador + 1; es decir, incrementa el valor de interador en 1. 
      //El bucle while se ejecutará mientras que el valor del interador sea menor o igual a 100. Si el valor del interador es mayor que 100, el bucle while se detendrá y el programa terminará.
+     
+        
 }
 }
 }
---
+++ c2-javabasics/src/UD05/T5ejercicio8.java
@@ -0,0 +1,35 @@
+package UD05;
+
+public class T5ejercicio8 {
+
+	public static void main(String[] args) {
+		// Este ejercicio me pide que haga el ejecio anterior(T5ejercicio7) pero con un bucle for en vez de un bucle while.
+		
+		// El bucle for es una estructura de control que se utiliza para repetir un bloque de código un número fijo de veces. En palabras faciles, el bucle for se utiliza para ejecutar un bloque de código un número determinado de veces.
+		
+		//A diferencia del bucle while, el bucle for tiene una inicialización, una condición de terminación y una expresión de incremento o decremento.
+		
+		// El bucle for se utiliza cuando se conoce el número de iteraciones que se van a realizar. 
+		//Por ejemplo, si queremos mostrar los números del 1 al 100 en la consola, podemos utilizar un bucle for. Para ello, vamos a seguir los siguientes pasos:
+		
+		// Paso 1: Crear un bucle for que imprima los números del 1 al 100.
+		
+		int iterador=1; //declaramos una variable entera para el bucle for que empiece en 1 y se llame interador.
+		
+		//Paso 2: Crear un bucle for que imprima los números del 1 al 100.
+		for (iterador = 1; iterador <= 100; iterador++) { // se pone { al final porque es un bloque de código que se va a repetir y para que el programa sepa donde empieza y donde termina el bloque de código.
+			
+			// El bucle for se ejecutará mientras que la condición sea verdadera. 
+			//En este caso, la condición es que el valor del interador sea menor o igual a 100.
+			// Si el valor del interador es mayor que 100, el bucle for se detendrá y el programa terminará.
+		
+		
+		// Paso 3: Mostrar el número actual del iterador.
+	
+		System.out.println(iterador);
+			
+			
+		}
+	}
+
+}
